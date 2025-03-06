c507a8037d98fb280b4557378b8199a3474caba2
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-06 10:12:34 +0200

    Add component Greeting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import Greeting from './components/Greeting';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting name='Nataliia' />
+    <Greeting name='Lidiia' />
+    <Greeting name='Olexandr' />
+    <Greeting name='Evgen' />
+  </>
 );
 
 
