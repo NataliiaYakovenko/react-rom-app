d2064944b683d1d906f463c647e52ce99d62a3cb
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-06 11:05:22 +0200

    Add component Greeting change state Hello/Bye

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,14 +4,35 @@ class Greeting extends React.Component{
     constructor(props){
         super(props);
          this.state = {
-
-         }
+            //1 створити змінну стейту (за прикладом лампочки true/false)
+            isGreeting: true
+        }
     }
-     // тут можуть бути якісь обробники подій
+     // 2 зробити обробник події кліку на h1 (за прикладом лампочки)
+      changeGreetHandler(){
+        this.setState({
+            isGreeting:!this.state.isGreeting
+        })
+
+        
+      }
+
 
      render(){
-        return <h1>Hello {this.props.name}</h1>
+        //3 в залежності від значення стейту, вирахувати, що за текс ви показуєте (за прикладом лампочки)
+        const greetChange = this.state.isGreeting ? 'Hello' : "Bye"
+
+        return <h1 onClick={()=>{this.changeGreetHandler()}}>{greetChange} {this.props.name}</h1>
+
+        //не забудьте підписати h1 на обробник кліку розроблений у пункті 2
      }
 }
 
 export default Greeting;
+
+/*
+Доробити компоненту таким чином, 
+щоб за натисканням на h1 текст Hello змінюється на Bye
+
+
+*/
