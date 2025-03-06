a44a689fca4f31672bd7bac59b594b68e0fe279f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-06 16:17:04 +0200

    Add map and sort in conponent AlohaDashboard

--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -1,40 +1,56 @@
 import React from "react";
 import Aloha from "../Aloha";
 
-const usersArray =[
-    {
-        firstName: "Nataliia",
-        lastName: "Yakovenko"
-    },
-    {
-        firstName: "Roland",
-        lastName: "Symonian"
-    },
-    {
-        firstName: "David",
-        lastName: "Brokovich"
-    },
-    {
-        firstName: "Taylor",
-        lastName: "Covalskiy"
-    },
-    {
-        firstName: "Jon",
-        lastName: "Travolte"
-    }
-]
-
-
 class AlohaDashboard extends React.Component {
-         userToAloha(){
-            return usersArray.map((user)=> <Aloha firstName={user.firstName} lastName={user.lastName} />)
-         }   
+  constructor(props) {
+    super(props);
+    this.state = {
+      usersArray: [
+        {
+          firstName: "Nataliia",
+          lastName: "Yakovenko",
+        },
+        {
+          firstName: "Roland",
+          lastName: "Symonian",
+        },
+        {
+          firstName: "David",
+          lastName: "Brokovich",
+        },
+        {
+          firstName: "Taylor",
+          lastName: "Covalskiy",
+        },
+        {
+          firstName: "Jon",
+          lastName: "Travolte",
+        },
+      ],
+    };
+  }
 
+  userToAloha() {
+    const { usersArray } = this.state;
+    return usersArray.map((user) => (
+      <Aloha firstName={user.firstName} lastName={user.lastName} />
+    ));
+  }
+
+  sortUsers() {
+    const { usersArray } = this.state;
+    const newArray = [...usersArray];
+    newArray.sort((a, b) => (a.firstName > b.firstName ? 1 : -1));
+    this.setState({
+        usersArray:newArray
+    })
+  }
 
   render() {
     return (
       <>
-       {this.userToAloha()}
+        <button onClick={() => {this.sortUsers()}} >Sort</button>
+        {this.userToAloha()}
       </>
     );
   }
