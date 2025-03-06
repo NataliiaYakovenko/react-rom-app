1a8b1b8b32731e972ad7f5a8a356d8303a17628f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-06 09:05:05 +0200

    Add component App лампочка

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      on: false,
+    };
+  }
+
+  toggleHandler(){
+      this.setState({
+        on:!this.state.on
+      });
+  }
+
+  render() {
+    const classFordiv = this.state.on ? "lamp-on" : "lamp-off";
+
+    return (
+      <>
+        <div className={classFordiv}></div>
+        <button onClick={()=>{this.toggleHandler()}}>Toggle</button>
+      </>
+    );
+  }
+}
+
+export default App;
+
+/*
+Створити коло лампочку і кнопку-вимикач
+За натисненням кнопки лампочка має підсвітитись наповнитись жовтим кольором
+
+*/
