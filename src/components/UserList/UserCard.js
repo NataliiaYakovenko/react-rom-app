ed14211ffdd8a08ef4ed4d74a4019eb6710775ee
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-18 21:20:29 +0200

    Add PropTypes

--- src/components/UserList/UserCard.js
+++ src/components/UserList/UserCard.js
@@ -1,21 +1,44 @@
-// В компоненті UserCard потрібно реалізувати логіку відмалювання одного
-//єдиного користувача 
-//Цей єдиний користувач спускається в компоненту UserCard через пропси
-//від батьківської компоненти UserList
-import './style.css'
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.css";
 
-function UserCard({user}){
-  
-    const {name:{first: firstName,last: lastName}, email, picture:{large:imgSrc}}  = user
+class UserCard extends React.Component {
 
-    return(
-        <article className='card-wrapper'>
-            <img src={imgSrc} alt={`${firstName} ${lastName}`} className='user-img' />
-            <h3>{firstName} {lastName}</h3>
-            <p>{email}</p>
-        </article>
-    )
+
+  render() {
+    const { user:{
+      name: { first: firstName, last: lastName },
+      email,
+      picture: { large: imgSrc },
+    }} = this.props;
+
+    return (
+      <article className="card-wrapper">
+        <img
+          src={imgSrc}
+          alt={`${firstName} ${lastName}`}
+          className="user-img"
+        />
+        <h3>
+          {firstName} {lastName}
+        </h3>
+        <p>{email}</p>
+      </article>
+    );
+  }
 }
 
-export default UserCard
+UserCard.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.shape({
+        first: PropTypes.string.isRequired,
+        last: PropTypes.string.isRequired
+    }).isRequired,
+    email: PropTypes.string.isRequired,
+    picture: PropTypes.shape({
+        large: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired,
+};
+
+export default UserCard;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TodoList from './components/TodoList/TodoList'
+import UserList from "./components/UserList/UserList";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TodoList />);
+root.render(<UserList />);
 
 
