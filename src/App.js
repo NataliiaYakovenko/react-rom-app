1eaa30316eaa0249d2cd07c87800d9433c64db80
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-25 22:30:50 +0200

    Add Toastic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
     <>
-    <button onClick={() => {window.location.assign('https://unsplash.com/s/photos/dogs')}}> Assign</button>
-    <button onClick={() => {window.location.reload()}}> Relosd</button>
-    <button onClick={() => {window.location.replace('https://pixabay.com/images/search/cat/')}}> Replace</button>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+     
+      />
 
+      <button
+        onClick={() => {
+          window.location.assign("https://unsplash.com/s/photos/dogs");
+        }}
+      >
+        {" "}
+        Assign
+      </button>
+
+      <button
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
+        {" "}
+        Relosd
+      </button>
+      <button
+        onClick={() => {
+          window.location.replace("https://pixabay.com/images/search/cat/");
+        }}
+      >
+        {" "}
+        Replace
+      </button>
+
+      <p>
+        Dogs are considered by many to be man’s best friend, and a lot of us
+        love to wonder about exactly what our loyal and energetic companions are
+        thinking or what they would say to us if they were talking animals.
+      </p>
+      <button
+        onClick={() => {
+          window.navigator.clipboard.writeText(
+            "Dogs are considered by many to be man’s best friend, and a lot of us love to wonder about exactly what our loyal and energetic companions are thinking or what they would say to us if they were talking animals."
+          );
+          toast.success("🦄 Wow so easy!");
+        }}
+      >
+        Click to copy text
+      </button>
     </>
   );
 }
