abab84a1a1df14a583d9a15b4cad6caee38f0070
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-26 22:51:39 +0200

    Add WindowResizer width, height viewport

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
 import React from "react";
-import { ToastContainer, toast } from "react-toastify";
+import WindowResizer from "./components/WindowResizer/WindowResizer";
+
 
 function App() {
   return (
-    <>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick={false}
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-
-      <button
-        onClick={() => {
-          window.location.assign("https://unsplash.com/s/photos/dogs");
-        }}
-      >
-        {" "}
-        Assign
-      </button>
-
-      <button
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        {" "}
-        Relosd
-      </button>
-      <button
-        onClick={() => {
-          window.location.replace("https://pixabay.com/images/search/cat/");
-        }}
-      >
-        {" "}
-        Replace
-      </button>
-
-      <p>
-        Dogs are considered by many to be man’s best friend, and a lot of us
-        love to wonder about exactly what our loyal and energetic companions are
-        thinking or what they would say to us if they were talking animals.
-      </p>
-      <button
-        onClick={() => {
-          window.navigator.clipboard.writeText(
-            "Dogs are considered by many to be man’s best friend, and a lot of us love to wonder about exactly what our loyal and energetic companions are thinking or what they would say to us if they were talking animals."
-          );
-          toast.success("🦄 Wow so easy!");
-        }}
-      >
-        Click to copy text
-      </button>
-
-      <button
-        onClick={() => {
-          window.navigator.clipboard.readText().then((text) => {
-            console.log(text);
-          });
-        }}
-      >
-        Read text from buffer
-      </button>
-
-      <button
-        onClick={() => {
-          window.navigator.geolocation.getCurrentPosition((myPosition) => {
-            console.log(myPosition);
-          });
-        }}
-      >
-        Get my current geopisition
-      </button>
-    </>
+   <WindowResizer/>
   );
 }
 
 export default App;
+
---
+++ src/components/WindowResizer/WindowResizer.js
@@ -0,0 +1,46 @@
+/*
+Напиати компоненту WindowResizer
+Яка виводить розмір вьюпорта на поточний момент
+*/
+import React, { Component } from "react";
+
+class WindowResizer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+  }
+ 
+  componentDidMount(){
+    window.addEventListener('resize', this.eventHandler)
+    this.eventHandler();
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.eventHandler)
+  }
+
+  eventHandler=()=>{
+    this.setState({
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
+  }
+
+
+
+  render() {
+  const {width,height} = this.state
+    
+    return <div>
+           <p>Current viewport width: {width}px</p>
+           <p>Current viewport height: {height}px</p>
+
+    </div>;
+  }
+}
+
+export default WindowResizer;
