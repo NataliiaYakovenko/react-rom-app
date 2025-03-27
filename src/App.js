e066f92ceaef48424b42e5a0bd2b5fa2956d9de5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-27 22:40:21 +0200

    Маршрутизація, Перемикання components,  React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from "react";
-import WindowResizer from "./components/WindowResizer/WindowResizer";
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
 
+import Counter from "./components/Counter/CounterParent";
+import UserList from "./components/UserList/UserList";
+import TodoList from "./components/TodoList/TodoList";
+import AlohaDashboard from "./components/AlohaDashboard/AlohaDashboard";
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-   <WindowResizer/>
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to='counter'>Go to Counter component</Link></li>
+            <li><Link to='userlist'>Go to UserList component</Link></li>
+            <li><Link to='todolist'>Go to TodoList component</Link></li>
+            <li><Link to='alohaDashboard'>Go to AlohaDashboard component</Link></li>
+    
+
+
+            
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/userlist" element={<UserList />} />
+          <Route path="/todolist" element={<TodoList />} />
+          <Route path="/alohaDashboard" element={<AlohaDashboard />} />
+          <Route path="/*" element={<NotFound />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
 
+/*
+SSR (server-side rendering) - додатки, які рендеряться на сервері
+
+CSR (client-side rendering) - додатки, які рендеряться на клієнті
+*/
