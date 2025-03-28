e2e4124c7191cecef5a40d99264d4783de50ab18
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-03-28 21:15:58 +0200

    Add PhonesLoader with phones

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
+import PhonesLoader from "./components/PhonesLoader/PhonesLoader";
 
-import Counter from "./components/Counter/CounterParent";
-import UserList from "./components/UserList/UserList";
-import TodoList from "./components/TodoList/TodoList";
-import AlohaDashboard from "./components/AlohaDashboard/AlohaDashboard";
-import Home from './components/Home/Home';
-import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <nav>
-          <ul>
-            <li><Link to='counter'>Go to Counter component</Link></li>
-            <li><Link to='userlist'>Go to UserList component</Link></li>
-            <li><Link to='todolist'>Go to TodoList component</Link></li>
-            <li><Link to='alohaDashboard'>Go to AlohaDashboard component</Link></li>
-    
-
-
-            
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/userlist" element={<UserList />} />
-          <Route path="/todolist" element={<TodoList />} />
-          <Route path="/alohaDashboard" element={<AlohaDashboard />} />
-          <Route path="/*" element={<NotFound />} />
-
-        </Routes>
-      </BrowserRouter>
-    </>
+    <PhonesLoader/>
   );
 }
 
 export default App;
 
-/*
-SSR (server-side rendering) - додатки, які рендеряться на сервері
-
-CSR (client-side rendering) - додатки, які рендеряться на клієнті
-*/
---
+++ src/components/PhonesLoader/PhonesLoader.js
@@ -0,0 +1,59 @@
+import React, {Component} from "react";
+
+class PhonesLoader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      phones: [],
+      isLoading: true,
+      isError: false,
+    };
+  }
+
+  componentDidMount = () => {
+    this.load();
+  };
+
+  load = () => {
+    fetch ("./phones.json")
+      .then(response => response.json())
+      .then((phones) => {
+        this.setState({
+          phones: phones,
+        })
+      })
+      .catch((err) => {
+        this.setState({
+          isError: err,
+        })
+      })
+      .finally(() => {
+        this.setState({
+          isLoading: false,
+        })
+     } );
+  };
+
+  render() {
+    const { phones, isLoading, isError } = this.state;
+    return (
+      <div>
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>Error...{isError.message}</div>}
+
+        <ul>
+          {phones.map((phone) => {
+            return (
+              <li>
+                Brand: {phone.brand}--- Model: {phone.model}--- Price: {phone.price}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default PhonesLoader;
