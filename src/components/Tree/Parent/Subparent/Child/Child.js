ed8061776cbaf390705245fa32817ca03f5bc752
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-02 23:12:22 +0300

    Add avatar in Child

--- src/components/Tree/Parent/Subparent/Child/Child.js
+++ src/components/Tree/Parent/Subparent/Child/Child.js
@@ -1,13 +1,26 @@
 import React from "react";
 import InnerChild from "./InnerChild/InnerChild";
+import { UserContext } from "../../../../context/UserContext";
+import styles from './Child.module.css'
+
 
 const Child = (props) => {
+  
   return (
-    <div >
-      Child
-      <InnerChild />
-    </div>
+    <UserContext.Consumer>
+      {({ user }) => {
+       return (<div>
+          Child
+          <p>
+            {user.firstName} {user.lastName}
+          </p>
+          <img src= {user.avatar} alt='This is avatar' className={styles.avatar}/>
+   
+          <InnerChild />
+        </div>);
+      }}
+    </UserContext.Consumer>
   );
-}
+};
 
 export default Child;
