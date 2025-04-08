f8c8d34d52ffca03f1c9c39e4082db7aac91beac
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-08 22:40:57 +0300

    Add Formik

--- src/components/FormSignUp/FormSignUp.js
+++ src/components/FormSignUp/FormSignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SIGN_UP_SHEMA from "../../schemas";
+import { Formik, Form, Field } from "formik";
 
 const initialState = {
   firstName: "",
@@ -8,85 +9,28 @@ const initialState = {
   password: "",
 };
 
-class FormSignUp extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      ...initialState,
-      isError: null,
-    };
-  }
-
-  changeHandler = ({ target: { value, name } }) => {
-    this.setState({
-      [name]: value,
-    });
+function FormSignUp(props) {
+ const handlerSubmitToFormik = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
   };
 
-  submitHandler = (event) => {
-    event.preventDefault();
-    try {
-      SIGN_UP_SHEMA.validateSync(this.state);
+  return (
+    <Formik initialValues={initialState} onSubmit={handlerSubmitToFormik}>
+      {(formikProps) => {
+        return (
+          <Form>
+            <Field type="text" name="firstName" placeholder='Your first name'/>
+            <Field type="text" name="lastName" placeholder='Your last name'/>
+            <Field type="text" name="email" placeholder='Your email'/>
+            <Field type="text" name="password" placeholder='Your password'/>
 
-      this.setState({
-        isError: null,
-      });
-    } catch (err) {
-      this.setState({
-        isError: err,
-      });
-    }
-  };
-
-  render() {
-    const { email, password, firstName, lastName, isError } = this.state;
-    return (
-      <form onSubmit={this.submitHandler}>
-        <input
-          type="text"
-          value={firstName}
-          placeholder="First name"
-          name="firstName"
-          onChange={this.changeHandler}
-        />
-
-        <input
-          type="text"
-          value={lastName}
-          placeholder="Last name"
-          name="lastName"
-          onChange={this.changeHandler}
-        />
-
-        <input
-          type="text"
-          value={email}
-          placeholder="Your login"
-          name="email"
-          onChange={this.changeHandler}
-        />
-
-        <input
-          type="text"
-          value={password}
-          placeholder="Your password"
-          name="password"
-          onChange={this.changeHandler}
-        />
-        <button>Login</button>
-
-        {isError && (
-          <p style={{ color: "red", fontSize: "20px" }}>{isError.message}</p>
-        )}
-      </form>
-    );
-  }
+            <button type="submit">Send</button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export default FormSignUp;
-
-/*
-Зробити компоненту входу
-Провести валідацію з використанням Yup
-*/
