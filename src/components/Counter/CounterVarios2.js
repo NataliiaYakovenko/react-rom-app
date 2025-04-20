20801e7c5312bf2ff6cb0840fd70ec77a4bf59a0
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-20 09:33:11 +0300

    Add Counter + Stopwatch

--- src/components/Counter/CounterVarios2.js
+++ src/components/Counter/CounterVarios2.js
@@ -1,36 +1,44 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { format, addSeconds } from "date-fns";
 
 function CounterVarios2() {
-  const [count, setCount] = useState(0);
-  const [isRunning, setIsRunning] = useState(true)
+  const [time, setTime] = useState(new Date(0, 0, 0, 0, 0, 0, 0)); // абстрактна нульова дата
+  const [isRunning, setIsRunning] = useState(true);
 
 
-  useEffect(() => {                        
- if(isRunning === true){
-    const intervalId = setInterval(() => {     // componentDidMount
-        setCount((count) => {
-          return count + 1;
+  useEffect(() => {
+    if (isRunning === true) {
+      const intervalId = setInterval(() => {
+        // componentDidMount
+        setTime((time) => {
+          return addSeconds(time, 1);
         });
       }, 1000);
-  
-      return ()=>{                       
-           clearInterval(intervalId)   // componentWillUnMount
-      }
- }
+
+      return () => {
+        clearInterval(intervalId); // componentWillUnMount
+      };
+    }
   }, [isRunning]);
 
- const switchRunning = ()=>{
-    setIsRunning(!isRunning)
- }
+  const switchRunning = () => {
+    setIsRunning(!isRunning);
+  };
 
+  const clearHandler = () => {
+    setTime(new Date(0, 0, 0, 0, 0, 0, 0));
+  };
 
-  return <>
-  <h1>{count}</h1>
-  <button onClick={switchRunning}>{isRunning ? 'Stop' : "Start"}</button>
-
-
-  </>
+  return (
+    <>
+      <h1>{format(time, "HH:mm:ss")}</h1>
+      <button onClick={switchRunning}>{isRunning ? "Stop" : "Start"}</button>
+      <button disabled={isRunning ? true : false} onClick={clearHandler}>
+        Clear
+      </button>
+    </>
+  );
 }
 
 export default CounterVarios2;
