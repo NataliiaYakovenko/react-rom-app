f9caafc032ddff5f83098b90656071d7ca92de5a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-23 22:18:53 +0300

    Add useMemo

--- src/components/SandboxComponent/Clicker.js
+++ src/components/SandboxComponent/Clicker.js
@@ -1,47 +1,31 @@
 import React from "react";
-import { useState, useCallback } from "react";
-
-//Підрахувати кількість натиснень на кнопку
+import { useState, useCallback, useMemo } from "react";
 
 function Clicker() {
   const [clickCount, setClickCount] = useState(0);
   const [inputValue, setInputValue] = useState("");
 
-  // const clickHandler =()=>{
-  //   setClickCount(clickCount + 1)
-  // }
-
   const clickHandler = useCallback(() => {
     setClickCount((clickCount) => clickCount + 1);
   }, []);
 
-  // const changeHandler =({target:{value,name}})=>{
-  //   setInputValue(Number(value))
-  // }
-
-  const changeHandler = useCallback(({ target: { value} }) => {
+  const changeHandler = useCallback(({ target: { value } }) => {
     setInputValue(Number(value));
   }, []);
 
-  //   const logValueHandler=()=>{
-  // let sum = 0
+  
+    const logValueHandler=()=>{
+  let sum = 0
 
-  // for(let i = 0; i<inputValue; i++){
-  //     sum += i
-  // }
-  //     console.log(sum)
-  //     console.log(inputValue)
-  //   }
-
-  const logValueHandler = useCallback(() => {
-    let sum = 0;
-
-    for (let i = 0; i < inputValue; i++) {
-      sum += i;
+  for(let i = 0; i<inputValue; i++){
+      sum += i
+  }
+    return sum
     }
-    console.log(sum);
-    console.log(inputValue);
-  }, [inputValue]);
+
+   const memoisedValue =  useMemo(()=>logValueHandler(),[inputValue])
+   console.log(memoisedValue)
+
 
   return (
     <>
