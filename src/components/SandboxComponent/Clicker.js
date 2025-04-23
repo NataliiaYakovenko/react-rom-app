6058309c009937ffed117a86868a9c1d2a867186
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-04-23 21:57:52 +0300

    Add useCallback

--- src/components/SandboxComponent/Clicker.js
+++ src/components/SandboxComponent/Clicker.js
@@ -5,6 +5,7 @@ import { useState, useCallback } from "react";
 
 function Clicker() {
   const [clickCount, setClickCount] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   // const clickHandler =()=>{
   //   setClickCount(clickCount + 1)
@@ -14,10 +15,40 @@ function Clicker() {
     setClickCount((clickCount) => clickCount + 1);
   }, []);
 
+  // const changeHandler =({target:{value,name}})=>{
+  //   setInputValue(Number(value))
+  // }
+
+  const changeHandler = useCallback(({ target: { value} }) => {
+    setInputValue(Number(value));
+  }, []);
+
+  //   const logValueHandler=()=>{
+  // let sum = 0
+
+  // for(let i = 0; i<inputValue; i++){
+  //     sum += i
+  // }
+  //     console.log(sum)
+  //     console.log(inputValue)
+  //   }
+
+  const logValueHandler = useCallback(() => {
+    let sum = 0;
+
+    for (let i = 0; i < inputValue; i++) {
+      sum += i;
+    }
+    console.log(sum);
+    console.log(inputValue);
+  }, [inputValue]);
+
   return (
     <>
       <h1>Click count: {clickCount}</h1>
       <button onClick={clickHandler}>Click</button>
+      <input type="text" value={inputValue} onChange={changeHandler} />
+      <button onClick={logValueHandler}>Click to log inputValue</button>
     </>
   );
 }
